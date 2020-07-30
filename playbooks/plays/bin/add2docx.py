1a63c4271733562b472e3d57108b36de41630cc0
jperezhcmp <jperezh@kionetworks.com>
2020-07-29 22:14:53 -0500

    construccion de la segunda  Grafica dos

--- playbooks/plays/bin/add2docx.py
+++ playbooks/plays/bin/add2docx.py
@@ -15,6 +15,10 @@ import matplotlib.pyplot as plt
 from matplotlib.patches import Shadow
 g_fufield='filtered_updates'
 g_ctgfield='categories'
+g_notWinfield="Windows"
+g_Title_for_G="Updates"
+g_Aspect_for_G="equal"
+g_Font_for_G={'fontsize': "x-large",'fontweight': 7,}
 nitemfield=0
 g_ctu=0
 g_ltu=[]
@@ -27,7 +31,8 @@ host = str(sys.argv[3])
 #sp = str(sys.argv[5])
 #ip = str(sys.argv[6])
 jsonFile = str(sys.argv[4])
-
+with open(jsonFile) as f:
+	g_dataG = json.load(f)
 #--Loading json file, cleaning useless columns to have an easier json file to work with
 
 with open(jsonFile) as data_file:
@@ -52,12 +57,9 @@ os = first_value['categories'][1]
 fig = plt.figure(figsize=(6, 6))
 ax = fig.add_axes([0.1, 0.1, 0.8, 0.8])
 labels = 'found_update_count', 'installed_update_count'
-
 foundupdate_count=da['found_update_count']
 installed_update_count=da['installed_update_count']
-
 fracs = [foundupdate_count, installed_update_count]
-
 explode = (0, 0.05)
 pies = ax.pie(fracs, explode=explode, labels=labels, autopct='%1.1f%%')
 for w in pies[0]:
@@ -71,6 +73,40 @@ for w in pies[0]:
 from io import BytesIO
 f = BytesIO()
 fig.savefig('/tmp/report_'+host+'.png')
+
+for elem in g_dataG[g_fufield]:
+	for g_st in g_dataG[g_fufield][str(elem)][g_ctgfield]: 
+		if g_st in g_ltu:
+			g_stu[str(g_st)]=1+g_stu[str(g_st)]
+			g_ctu=g_ctu+1
+		if ((g_notWinfield not in g_st) and (g_st not in g_ltu)):
+			g_ltu.append(str(g_st))
+			g_stu[str(g_st)]=1
+			g_ctu=g_ctu+1
+fig, ax = plt.subplots(figsize=(9, 5), subplot_kw=dict(aspect=g_Aspect_for_G))
+g_albls=[]
+g_asizes=[]
+for g_k in g_stu:
+    g_albls.append(str(g_k))
+    g_asizes.append(g_stu[str(g_k)])
+g_labels = g_albls
+g_fracs = g_asizes
+fig = plt.figure(figsize=(8, 6))
+ax = fig.add_axes([0.1, 0.1, 0.8, 0.8])
+g_explode = (0, 0,0,0)
+g_pies = ax.pie(g_fracs, labels=g_labels, explode=g_explode, autopct='%.2f')
+for w in g_pies[0]:
+    w.set_gid(w.get_label())
+    w.set_edgecolor("none")
+for w in g_pies[0]:
+    s = Shadow(w, -0.01, -0.01)
+    s.set_gid(w.get_gid() + "_shadow")
+    s.set_zorder(w.get_zorder() - 0.1)
+    ax.add_patch(s)
+ax.set_title(g_Title_for_G,fontdict=g_Font_for_G)
+from io import BytesIO
+f = BytesIO()
+fig.savefig('/tmp/report_categories_'+host+'.png')
 
 #os = 'Windows Server 2008 R2'
 
