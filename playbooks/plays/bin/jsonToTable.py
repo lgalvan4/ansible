bc3907608905c6722b0d039e37145e6f5c8537ae
Eddy Nava <edgardoarturo.nava@gmail.com>
2020-07-31 10:12:50 -0500

    tengo sueño

--- playbooks/plays/bin/jsonToTable.py
+++ playbooks/plays/bin/jsonToTable.py
@@ -3,10 +3,12 @@ import sys
 import datetime
 from os import path
 import json
+from pandas.io.json import json_normalize
 
 #python-docx
 from docx import Document
 from docx.enum.text import WD_ALIGN_PARAGRAPH
+from docx.enum.table import WD_TABLE_ALIGNMENT
 from docx.shared import Pt
 from docx.shared import Inches
 from docx.enum.style import WD_STYLE_TYPE
@@ -32,20 +34,58 @@ jsonFile = str(sys.argv[4])
 
 with open(jsonFile) as data_file:
     da = json.load(data_file)
-    data = json.dumps(da['updates'])
-    duckingUpdates = json.loads(data)
 
-#--Taking KB Information out of the title to avoid displaying duplicated info
+for key in da['updates']:
 
-for ftitle in duckingUpdates.values():
-    borrarKB = ftitle['title']
+    da['updates'][str(key)]['categories'] = da['updates'][str(
+        key)]['categories'][0]
+
+    if da['updates'][str(key)]['categories'] == "Critical Updates":
+        da['updates'][str(key)]['fsorting'] = 1
+    elif da['updates'][str(key)]['categories'] == "Security Updates":
+        da['updates'][str(key)]['fsorting'] = 2
+    elif da['updates'][str(key)]['categories'] == "Update Rollups":
+        da['updates'][str(key)]['fsorting'] = 3
+    elif da['updates'][str(key)]['categories'] == "Updates":
+        da['updates'][str(key)]['fsorting'] = 4
+    elif da['updates'][str(key)]['categories'] == "Feature Packs":
+        da['updates'][str(key)]['fsorting'] = 5
+    elif da['updates'][str(key)]['categories'] == "Tools":
+        da['updates'][str(key)]['fsorting'] = 6
+    elif da['updates'][str(key)]['categories'] == "Application":
+        da['updates'][str(key)]['fsorting'] = 7
+    elif da['updates'][str(key)]['categories'] == "Connectors":
+        da['updates'][str(key)]['fsorting'] = 8
+    elif da['updates'][str(key)]['categories'] == "Definition Updates":
+        da['updates'][str(key)]['fsorting'] = 9
+    elif da['updates'][str(key)]['categories'] == "Developer Kits":
+        da['updates'][str(key)]['fsorting'] = 10
+    elif da['updates'][str(key)]['categories'] == "Guidance":
+        da['updates'][str(key)]['fsorting'] = 11
+
+data = json.dumps(da['updates'])
+fuckingUpdates = json.loads(data)
+
+for fckupdates in fuckingUpdates.values():
+    borrarKB = fckupdates['title']
     kbBorrado = borrarKB.rsplit(' ', 1)[0]
-    ftitle['title'] = kbBorrado
+    fckupdates['title'] = kbBorrado
+
+#print(fuckingUpdates)
+lista = []
+for value in fuckingUpdates.values():
+    lista.append(value)
+
+#print(lista)
+
+duckingUpdates = sorted(lista, key=lambda i: i['fsorting'])
 
 #--Getting Os Name
-iterator = iter(duckingUpdates.values())
-first_value = next(iterator)
-os = first_value['categories'][1]
+# iterator = iter(duckingUpdates.values())
+# first_value = next(iterator)
+# os = first_value['categories'][1]
+
+os = "Windows Server 2008 R2"
 
 #Graph
 fig = plt.figure(figsize=(6, 6))
@@ -67,7 +107,7 @@ for w in pies[0]:
     s.set_gid(w.get_gid() + "_shadow")
     s.set_zorder(w.get_zorder() - 0.1)
     ax.add_patch(s)
-f = BytesIO()
+
 fig.savefig('/tmp/report_'+host+'.png')
 
 #os = 'Windows Server 2008 R2'
@@ -141,7 +181,7 @@ records = (
 )
 
 table = document.add_table(rows=0, cols=2)
-table.style = document.styles['Light List Accent 6']
+table.style = document.styles['Medium Grid 1 Accent 1']
 #table.style = "Table Grid"
 #hdr_cells = table.rows[0].cells
 #hdr_cells[0].text = 'Id'
@@ -155,18 +195,20 @@ document.add_paragraph()  # Enter
 document.add_paragraph()  # Enter
 
 #Adding Graph iside a table
-table_graph = document.add_table(rows=1, cols=2)
+table_graph = document.add_table(rows=1, cols=1)
 
 #Celda de imagen
-pic_cells = table_graph.rows[0].cells
-pic_cell = pic_cells[0]
-run = pic_cell.add_paragraph().add_run()
-picture_path = '/tmp/report_'+host+'.png'
-run.add_picture(picture_path, width=Inches(3))
+# pic_cells = table_graph.rows[0].cells
+# pic_cell = pic_cells[0]
+# run = pic_cell.add_paragraph().add_run()
+# picture_path='/tmp/report_'+host+'.png'
+# run.add_picture(picture_path, width=Inches(3))
 
 #Celda de texto
 pic_cells2 = table_graph.rows[0].cells
-pic_cells2_run = pic_cells2[1].add_paragraph().add_run()
+pic_cells2_pag = pic_cells2[0].add_paragraph()
+pic_cells2_pag.alignment = WD_ALIGN_PARAGRAPH.CENTER
+pic_cells2_run = pic_cells2_pag.add_run()
 #pic_cells2_run.add_picture(picture_path, width=Inches(3))
 
 #tb_cell_run.font.size =  Pt(8)
@@ -176,37 +218,38 @@ document.add_paragraph()  # Enter
 
 #--Creating table - Autofit didn't work - setting up columns width manually
 reportTable = document.add_table(rows=1, cols=4)
-reportTable.autofit = True
-reportTable.style = 'Colorful Shading Accent 6'
+#reportTable.autofit = False
+#reportTable.style = 'Colorful Shading Accent 6'
+reportTable.style = 'Medium Grid 1 Accent 1'
 hdr_Cells = reportTable.rows[0].cells
-#hdr_Cells[0].width = Inches(0.48)
+hdr_Cells[0].width = Inches(0.48)
 hdr_Cells[0].text = 'KB'
-#hdr_Cells[1].width = Inches(4.45)
+hdr_Cells[1].width = Inches(4.30)
 hdr_Cells[1].text = 'Title'
-#hdr_Cells[2].width = Inches(0.77)
+hdr_Cells[2].width = Inches(0.77)
 hdr_Cells[2].text = 'Category'
-#hdr_Cells[3].width = Inches(0.45)
+hdr_Cells[3].width = Inches(0.45)
 hdr_Cells[3].text = 'Installed'
 
 #--Filling up table
 catgories_dict = {}
 
-for fvalue in duckingUpdates.values():
+for fvalue in duckingUpdates:
     row_Cells = reportTable.add_row().cells
-    #row_Cells[0].width = Inches(0.48)
+    row_Cells[0].width = Inches(0.48)
     row_Cells[0].text = fvalue['kb']
-    #row_Cells[1].width = Inches(4.45)
+    row_Cells[1].width = Inches(4.30)
     row_Cells[1].text = fvalue['title']
-    #row_Cells[2].width = Inches(0.77)
-    row_Cells[2].text = fvalue['categories'][0]  # count sobre esta linea
-    #row_Cells[3].width = Inches(0.45)
+    row_Cells[2].width = Inches(0.77)
+    row_Cells[2].text = fvalue['categories']  # count sobre esta linea
+    row_Cells[3].width = Inches(0.45)
     row_Cells[3].text = str(fvalue['installed'])
 
-    if fvalue['categories'][0] in catgories_dict:
-        catgories_dict[fvalue['categories'][0]] = catgories_dict.get(
-            fvalue['categories'][0])+1
+    if fvalue['categories'] in catgories_dict:
+        catgories_dict[fvalue['categories']] = catgories_dict.get(
+            fvalue['categories'])+1
     else:
-        catgories_dict[fvalue['categories'][0]] = 1
+        catgories_dict[fvalue['categories']] = 1
 
 #print(catgories_dict)
 
@@ -219,18 +262,20 @@ contadores = []
 explod = []
 fcolors = []
 for t, c in catgories_dict.items():
-    titulos.append(t)
+    titulos.append(t+' '+str(c))
     contadores.append(c)
     explod.append(0.05)
 
     if t == 'Critical Updates':
-        fcolors.append("#C70039")
+        fcolors.append("#EC1E1E")
     elif t == 'Security Updates':
-        fcolors.append("#FF5733")
+        fcolors.append("#F3951C")
     elif t == 'Update Rollups':
-        fcolors.append("#FFC300")
+        fcolors.append("#2672BF")
     elif t == 'Updates':
-        fcolors.append("#F0E68C")
+        fcolors.append("#48B14B")
+    elif t == 'Service Packs':
+        fcolors.append("#F6F167")
     else:
         fcolors.append(fcolor)
 
@@ -238,22 +283,24 @@ for t, c in catgories_dict.items():
 #print(contadores)
 #print(explode)
 
-fig1, ax1 = plt.subplots()
-patches = ax1.pie(contadores, explode=tuple(explod), labels=tuple(
-    titulos), colors=tuple(fcolors), shadow=True, startangle=90)
+labels2 = tuple(titulos)
+fig1 = plt.figure(frameon=False)
+ax1 = fig1.add_axes([0, 0.2, 0.6, 0.6])
+patches, texts = ax1.pie(contadores, explode=tuple(
+    explod), colors=tuple(fcolors), startangle=90)
 #ax1.pie(contadores, explode=tuple(explod), labels=tuple(titulos), colors=tuple(fcolors) autopct='%1.1f%%', shadow=True, startangle=90)
-plt.legend(pie[0], patches, tuple(titulos), loc="best")
-#plt.legend(bbox_to_anchor=(1.05, 1.0), loc='upper left')
-plt.tight_layout()
-ax1.axis('equal')   # Equal aspect ratio ensures that pie is drawn as a circle.
+ax1.legend(patches, labels2,
+           loc="center left",
+           prop={'size': 10},
+           bbox_to_anchor=(1, 0, 0.5, 1))  # Equal aspect ratio ensures that pie is drawn as a circle.
 
 #plt.show()
-fig1.savefig('/tmp/report_'+host+'2.png')
+fig1.savefig('/tmp/report_'+host+'2.png', bbox_inches='tight', pad_inches=0)
 
 picture_path = '/tmp/report_'+host+'2.png'
-pic_cells2_run.add_picture(picture_path, width=Inches(3))
-
+pic_cells2_run.add_picture(picture_path, width=Inches(4))
 #-- Setting font size for the whole table.
+
 for row in reportTable.rows:
     for cell in row.cells:
         paragraphs = cell.paragraphs
